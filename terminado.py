cc6f3e32ecb1e8509b42c3f1722b70f35f54b5ff
Edwin Jeremias Agustin Yack <edwinjeremiasagustinyack@gmail.com>
2023-06-02 20:53:15 -0400

    quite mi key api y traduje los comentarios al ingles

--- terminado.py
+++ terminado.py
@@ -3,25 +3,25 @@ from gtts import gTTS
 import playsound 
 from time import sleep as sl 
 
-#descarga las librerias importadas en la terminal antes de probar 
+#dowloand the librerys after run the code: 
 #pip install openai --- pip install gtts --- pip install playsound
 intro = print('CHATGPT + AUDIO')
 sl(2)
 intro = print('create by Edwin')
 
 
-openai.api_key = "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"
+openai.api_key = " " #add one api_key of one openai account 
 dato = input('escribe tu pregunta: ')
 print('procesando...')
 
-#prompt es donde colocas las preguntas:
+#prompt is for write the cuestions:
 ai = openai.Completion.create( engine='text-davinci-003',
                                      prompt= dato,
                                      max_tokens = 1000
                                       )
 
 
-#se guarda la respuesta
+#save the answer:
 respuesta = str(ai.choices[0].text)
 
 
