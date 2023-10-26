d915d827d506e10acbab4e2fbc4867e067d1fd0c
Edoruin <edwinjeremiasagustinyack@gmail.com>
2023-10-26 15:08:46 -0400

    add the .bat file to the github

--- CHATGPT+AUDIO.bat
+++ CHATGPT+AUDIO.bat
@@ -1,2 +1,4 @@
-start " C:\Users\Jaime Farrel\Desktop\chatgpt" python terminado.py
-
+ @echo off 
+"C:\Users\Jaime Farrel\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.10"
+"C:\Users\Jaime Farrel\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\Jaime Farrel\Desktop\edwin\chatgpt\chatgpt+audio.py"
+exit
